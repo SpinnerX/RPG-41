4781b40eb794ead0e001d6119a6c2ac2f13a2ece
Corben Beaty <corbenbeaty@corb-macbook.local>
2023-03-01 14:29:51 -0800

    Updated

--- Game/common/GameCharacters.h
+++ Game/common/GameCharacters.h
@@ -6,11 +6,15 @@ class GameCharacter{
 public:
     GameCharacter() = default;
 
+    virtual void update(float x, float y) = 0;
+
     virtual ~GameCharacter() { delete texture; }
 
     virtual sf::FloatRect getBoundaries() = 0;
 
     virtual void draw(sf::RenderWindow* window) = 0;
+
+    virtual sf::Vector2f getPosition() = 0;
 
 protected:
     sf::RectangleShape bodyShape;
